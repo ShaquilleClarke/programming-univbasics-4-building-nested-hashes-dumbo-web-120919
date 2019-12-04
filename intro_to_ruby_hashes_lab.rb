cedc9d09f6116fc8655f94510c53ca82b1cba3fc
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-04 21:01:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def base_hash
   # keyword "return." Nice and neat.
   monopoly = {
     :railroads => {
-      
+
     }
   }
 end
@@ -11,7 +11,7 @@ end
 def monopoly_with_second_tier
   monopoly = {
     :railroads => {
-      
+
     }
   }# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
